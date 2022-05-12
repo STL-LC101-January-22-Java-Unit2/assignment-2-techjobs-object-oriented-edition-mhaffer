7d0dfe0f313173df0eb2c3639966f223be375389
mhaffer <maddison.haffer@gmail.com>
2022-05-12 10:14:26 -0500

    "second commit - Job complete, fixed spelling errors"

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -17,12 +17,12 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
-    Public Job() {
+    public Job() {
         id = nextId;
         nextId++;
     }
 
-    Public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
         this();
         this.name = name;
         this.employer = employer;
