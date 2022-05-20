ce2de2542e28c73cbe75fdd747f28c02a2ba1255
mhaffer <maddison.haffer@gmail.com>
2022-05-20 10:53:53 -0500

    "44/47 passing tests"

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -2,7 +2,7 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public abstract class JobField {
+public abstract class JobField { 
     private int id;
     private static int nextId = 1;
     private String value;
